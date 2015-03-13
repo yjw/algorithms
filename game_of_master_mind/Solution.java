b8c5bbeaa0bbacda9020b1a3232a05e36a5a309a
Yujue <yuchuehwang@gmail.com>
2015-03-12 22:18:49 -0700

    Added new questions

---
+++ game_of_master_mind/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] calculate (String guess, String slots) {
+        int[] hits = new int[2];
+        // hits[0] is the true hits;
+        // hits[1] is the pseudo hits.
+        for (int i = 0; i < guess.length(); i++) {
+            
+        }
+    }
+}
