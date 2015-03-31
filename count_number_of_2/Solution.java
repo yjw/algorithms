d0934a10abd223849c3ca2e59a5c4e6054b8f642
ywang <yuchuehwang@gmail.com>
2015-03-31 11:31:07 -0700

    finished count number of twos

--- count_number_of_2/Solution.java
+++ count_number_of_2/Solution.java
@@ -19,9 +19,33 @@ public class Solution {
     }
     
     public int count (int num) {
-        
+        int count = 0;
+        int len = Integer.toString(num).length();
+        for (int i = 0; i < len; i++) {
+            
+            count += digit_count(num, i);
+            //System.out.println("***count is " + count);
+        }
+        return count;
     }
     
-    private int 
-    
+    private int digit_count(int num, int i) {
+        String str = Integer.toString(num);
+        assert i < Integer.toString(num).length() && i >= 0;
+        int poweroften = (int)Math.pow(10, i);
+        int nextpoweroften = poweroften * 10;
+        int rounddown = num / nextpoweroften; // 2
+        int roundup = (num + nextpoweroften) / nextpoweroften ; // 
+        //System.out.println("round down is " + rounddown);
+        //System.out.println("round up is " + roundup);
+        if (str.charAt(str.length() - i - 1) < '2') {
+            return rounddown * poweroften;
+        }
+        if (str.charAt(str.length() - i - 1) > '2') {
+            return roundup * poweroften;
+        }
+        else {
+            return rounddown * poweroften + num % poweroften + 1;
+        }
+    }
 }
--- count_number_of_2/Test.java
+++ count_number_of_2/Test.java
@@ -1,6 +1,15 @@
 public class Test {
     public static void main (String[] args) {
         Solution s = new Solution();
-        System.out.println(s.brute(29));
+        for (int i = 0; i < 6553556; i++) {
+            if (s.brute(i) != s.count(i)) {
+                System.out.println("error");
+                //System.out.println(i);
+                //System.out.println("brute is " + s.brute(i));
+                //System.out.println("count is " + s.count(i));
+                return;
+            }
+        }
+        System.out.println("Got it!");
     }
 }
