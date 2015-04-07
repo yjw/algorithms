ce1a858e17ad478084f94dc2215e026670d97d1e
ywang <yuchuehwang@gmail.com>
2015-04-07 12:15:57 -0700

    Finished Inorder successor of a VST

--- tree/BST/InorderSuccessor.java
+++ tree/BST/InorderSuccessor.java
@@ -1,12 +1,25 @@
 public class InorderSuccessor {
-    public TreeNode successor (TreeNode root) {
-        if (root == null) {
+    public TreeNode successor (TreeNode root, TreeNode node) {
+        if (root == null || node == null) {
             return null;
         }
-        if (root.right != null) {
-            return min_node(root.right);
+        if (node.right != null) {
+            return min_node(node.right);
         }
-        
+        TreeNode succ = null;
+        while (root != null) {
+            if (node.val < root.val) {
+                succ = root;
+                root = root.left;
+            }
+            else if (node.val > root.val) {
+                root = root.right;
+            }
+            else {
+                break;
+            }
+        }
+        return succ;
     }
     
     private TreeNode min_node (TreeNode root) {
