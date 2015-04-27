deb09628420f3b8a25954dcf69d101af437ac547
ywang <yuchuehwang@gmail.com>
2015-04-26 18:44:26 -0700

    Added maximum subtree

---
+++ tree/max_subtree/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public TreeNode largest (TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        // It is actually post order traversal
+    }
+}
