a5eeb8aa34e21a676e1c02b212394d31add86be9
ywang <yuchuehwang@gmail.com>
2015-04-27 16:56:01 -0700

    Added build H2O

--- concurrency/H2O/Hydrogen.java
+++ concurrency/H2O/Hydrogen.java
@@ -11,6 +11,46 @@ The idiom to use lock
 
 */
 
+/*
+ class BoundedBuffer {
+   final Lock lock = new ReentrantLock();
+   final Condition notFull  = lock.newCondition(); 
+   final Condition notEmpty = lock.newCondition(); 
+
+   final Object[] items = new Object[100];
+   int putptr, takeptr, count;
+
+   public void put(Object x) throws InterruptedException {
+     lock.lock();
+     try {
+       while (count == items.length)
+         notFull.await();
+       items[putptr] = x;
+       if (++putptr == items.length) putptr = 0;
+       ++count;
+       notEmpty.signal();
+     } finally {
+       lock.unlock();
+     }
+   }
+
+   public Object take() throws InterruptedException {
+     lock.lock();
+     try {
+       while (count == 0)
+         notEmpty.await();
+       Object x = items[takeptr];
+       if (++takeptr == items.length) takeptr = 0;
+       --count;
+       notFull.signal();
+       return x;
+     } finally {
+       lock.unlock();
+     }
+   }
+ }
+*/
+
 public class Hydrogen {
     Hydrogen() {
         lock.lock();
@@ -27,6 +67,26 @@ public class Hydrogen {
             }
         }
         aH--;
-        lock.release();
+        lock.unlock();
     }
+}
+
+    Oxygen() {
+        lock.acquire();
+        wO++;
+        while (aO == 0) {
+            // try to make a water molecule
+            if (wH >= 2 && wO >= 1) {
+                wH-=2; aH+=2;
+                wO-=1; aO+=1;
+                waitingH.signal();
+                waitingH.signal();
+            }
+            // else wait for somebody else to
+            else {
+                waitingO.wait();
+            }
+        }
+        aO--;
+        lock.release();
 }
