1c9fbe52896e2dd33a315f264cf3cdef56feba73
Yujue <yuchuehwang@gmail.com>
2015-05-20 21:53:27 -0700

    tree to doubly linked list

---
+++ tree/tree_to_doubly_ls/ListNode.java
@@ -0,0 +1,8 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode (int value) {
+        val = value;
+        next = null;
+    }
+}
---
+++ tree/tree_to_doubly_ls/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public ListNode connect (TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+    }
+}
