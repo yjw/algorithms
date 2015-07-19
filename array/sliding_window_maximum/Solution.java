44b2b9681ea1921d1047e0551da657eab7dbd39f
ywang <yuchuehwang@gmail.com>
2015-07-19 00:34:14 -0700

    sliding window maximum

--- array/sliding_window_maximum/Solution.java
+++ array/sliding_window_maximum/Solution.java
@@ -1,6 +1,15 @@
+import java.util.PriorityQueue;
+
 public class Solution {
 	public int[] maxSlidingWindow(int[] nums, int k) {
 		assert(k >= 1 && k <= nums.length);
-		
+	    PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
+            queue.offer(nums[i]);
+            if (max < nums[i]) {
+                    max = nums[i];
+            }
+        }
 	}
 }
