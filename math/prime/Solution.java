d1e5eadb3a9242030ab780b6049a182c75cc0e9f
Yujue Wang <ywang@wkstn-ywang.west.isilon.com>
2015-07-30 14:02:45 -0700

    added primes

--- math/prime/Solution.java
+++ math/prime/Solution.java
@@ -1,8 +1,22 @@
 public class Solution {
     public int countPrimes(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        for (int i = 2; i < n + 1; i++) {
-            
+        assert(n >= 0);
+		boolean[] nPrime = new boolean[n];
+        int count = 0;
+		for (int i = 2; i < n; i++) {
+            if (nPrime[i]) {
+				continue;
+			}
+			for (int j = 2; i * j < n; j++) {
+				nPrime[i * j] = true;
+			}
         }
+
+		for (int i = 2; i < n; i++) {
+			if (!nPrime[i]) {
+				count++;
+			}
+		}
+		return count;
     }
 }
