4db7ec4207dc0026ebc51b92fe4a0a667d925e27
Yujue Wang <ywang@wkstn-ywang.west.isilon.com>
2015-08-07 11:39:29 -0700

    working on general dp

---
+++ dp/stack_of_boxes/Box.java
@@ -0,0 +1,10 @@
+public class Box {
+    public int length;
+    public int width;
+    public int height;
+    Box(int a, int b, int c) {
+        length = a;
+        width = b;
+        height = c;
+    }
+}
---
+++ dp/stack_of_boxes/Solution.java
@@ -0,0 +1,8 @@
+/**
+  * It is important to note that the transitivity
+  * of boxes. 
+*/
+
+public class Solution {
+	public void     
+}
