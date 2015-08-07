5b3fd9d0d5fd91d4ee5a3228470f5900e408ca1f
Yujue Wang <yujuewang@Yujues-MacBook-Pro.local>
2015-08-07 15:35:14 -0700

    working on make palindrome

--- string/match/palindrome/make_palindrome/Solution.java
+++ string/match/palindrome/make_palindrome/Solution.java
@@ -1,45 +1,5 @@
 public class Solution {
-    
-    /*
-    public String getShortestPalindrome(String word) {
-        if (word == null || word.equals("")) {
-            return word;
-        }
+    public String shortestPalindrome(String s) {
         
-        int n = word.length();
-        int left = 0;
-        int right = n - 1;
-        boolean previousCharWasMismatch = false;
-        StringBuilder appendOnRight = new StringBuilder(n);
-        StringBuilder matchedOnRight = new StringBuilder(n);
-        
-        while (left < right) {
-            char leftChar = word.charAt(left);
-            char rightChar = word.charAt(right);
-            
-            if (leftChar == rightChar) {
-                matchedOnRight.append(leftChar);
-                ++left;
-                --right;
-                c = false;
-            }
-            else {
-                appendOnRight.append(matchedOnRight);
-                matchedOnRight.setLength(0);
-                right = n - 1;
-                /*
-                if (previousCharWasMismatch) {
-                    appendOnRight.append(leftChar);
-                    ++left;
-                }
-                previousCharWasMismatch = true;
-                */
-            }
-        }
-        
-        String shortestPalindrome = word + appendOnRight.reverse();
-        return shortestPalindrome;
-
     }
-*/
 }
