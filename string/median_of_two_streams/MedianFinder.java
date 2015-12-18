5ce674645656c8498b91f838c6948d0ee5fc1439
yjw <yuchuehwang@gmail.com>
2015-12-17 22:38:25 -0800

    Added find median of a data stream. A very straightforward solution. Two ways to handle the reverse order.

---
+++ string/median_of_two_streams/MedianFinder.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+
+class MedianFinder {
+    private PriorityQueue<>
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        
+    }
+    // Returns the median of current data stream
+    public double findMedian() {
+    }
+};
