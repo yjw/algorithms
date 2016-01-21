aa49b75d87b89cdff82d24f41e986d52da995ad1
Yujue Wang <yuchuehwang@gmail.com>
2016-01-20 21:30:11 -0800

    revise the reservior sampling a little bit

---
+++ math/random/reservior/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] sample (int[] array, int k) {
+        int[] res = new int[k];
+        for (int i = k; i < array.length; i++) {
+            
+        }
+    } 
+}
